231d8095d2a7711087ebbd4a3fa2908dcb6aad8b
dengxiaodi <dengxiaodi@dmu.edu.cn>
2017-05-05 20:10:23 +0800

    index page

--- Application/Seqdb/Controller/IndexController.class.php
+++ Application/Seqdb/Controller/IndexController.class.php
@@ -9,10 +9,21 @@ class IndexController extends Controller {
      * 后台首页
      * @author 麦当苗儿 <zuojiazi@vip.qq.com>
      */
-    public function Index(){
+    public function index(){
         $time = time();
         $this->assign('time',$time);
-        $this->display('Index');
+
+        $Sample = D('Sample');
+        $Library = D('Library');
+        $Seq = D('Seq');
+        $SeqData = D('SeqData');
+
+        $this->assign('sample_count', $Sample->count());
+        $this->assign('library_count', $Library->count());
+        $this->assign('seq_count', $Seq->count());
+        $this->assign('seqdata_count', $SeqData->count());
+
+        $this->display('index');
     }
 
     /* 退出登录 */
@@ -21,7 +32,7 @@ class IndexController extends Controller {
             D('Member')->logout();
             $this->success('退出成功！', U('Index'));
         } else {
-            $this->redirect('Index');
+            $this->redirect('index');
         }
     }
 
